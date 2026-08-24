6fbe1d8d319628f7e7d5809c40b918bf32e46d0c
vpmicha <vpmicha@gmail.com>
2026-08-24 19:26:14 +0300

    created a better two_num_sum with O(n**2) complexity still and fixed possible bug by removing list name from parameters and adding numbers

--- Algos.py
+++ Algos.py
@@ -12,11 +12,11 @@ non_numbers = ['cat', 'dog', 'mouse', 'lion']
 
 floaters = [42.2, -7, 0, 15.5, 15, 99, -100.1, 42, 8.8, 1]
 
-def largest_number(list):
+def largest_number(numbers):
     found_number = False
-    if list:
-        number = list[0]
-        for i in list:
+    if numbers:
+        number = numbers[0]
+        for i in numbers:
             try:
                 if i >= number:
                     number = i
@@ -33,12 +33,12 @@ def largest_number(list):
 
     return number
 
-def second_largest_number(list):
+def second_largest_number(numbers):
     found_number = False
-    if list:
+    if numbers:
         largest = -inf
-        second_largest = list[0]
-        for i in list:
+        second_largest = numbers[0]
+        for i in numbers:
             try:
                 if i > largest:
                     second_largest = largest
@@ -62,12 +62,12 @@ def second_largest_number(list):
     else:
         return 'Empty list'
 
-def smallest_largest(list):
+def smallest_largest(numbers):
     found_number = False
-    if list:
+    if numbers:
         largest = -inf
         smallest = +inf
-        for i in list:
+        for i in numbers:
             try:
                 if i > largest:
                     largest = i
@@ -89,11 +89,11 @@ def smallest_largest(list):
     else:
         return 'Empty list'
 
-def value_count(list):
+def value_count(numbers):
     count_dict = {}
-    if list:
+    if numbers:
         first_time = True
-        for number in list:
+        for number in numbers:
             if number not in count_dict:
                 count_dict[number] = 1
             elif number in count_dict:
@@ -103,36 +103,24 @@ def value_count(list):
 
     return count_dict
 
-def two_num_sum(list, target_value):
+def two_num_sum(numbers, target_value):
     target = target_value
-    sum_list = list
-    if list:
-        first = list[0]
-        second = sum_list[0]
-        first_index = list.index(first)
-        first_loop_iterations = 0
-        second_loop_iterations = 0
-        while True:
-            for i in list[first_index:]:
-                first = i
-                first_loop_iterations += 1
-                break
-            for j in sum_list:
-                second = j
-                second_loop_iterations += 1
-                if first + second == target and first_loop_iterations != second_loop_iterations:
-                    return (first, second)
-            second_loop_iterations = 0
-            first_index += 1
-
-            if first_loop_iterations == len(list):
-                break
-
-        return 'No combination found'
-    return 'Empty list'
-
-[4, 6, 3, 6, 1, 7]                   
-[4, 6, 3, 6, 1, 7]                   
+    combinations = []
+    if numbers:
+        for number in numbers:
+            goal_number = target - number
+            if goal_number == number and numbers.count(number) > 1 and (goal_number, number) not in combinations:
+                combinations.append((goal_number, number))
                 
+            elif goal_number in numbers and (goal_number, number) not in combinations and (number, goal_number) not in combinations and goal_number != number:
+                combinations.append((goal_number, number))
 
-print(two_num_sum([4, 6, 3, 1, 1, 7], 12))
+            else:
+                continue
+        if combinations:
+            return combinations
+        else:
+            return 'No combination found'
+    return 'Empty List'
+
+print(two_num_sum([1, 4], 8))
